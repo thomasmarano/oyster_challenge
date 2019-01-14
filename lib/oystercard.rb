db60e39a246c47f2454f01bd1785b605f65782be
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-14 17:32:03 +0000

    Refactored top_up and deduct methods to meet single responsibility and to tidy up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -9,17 +9,21 @@ MAXIMUM_BALANCE = 90
   end
 
   def top_up(amount)
-    raise error_message if (amount + @balance) > MAXIMUM_BALANCE
-    @balance = @balance + amount
+    raise error_message if balance_exceeds_limit?(amount)
+    @balance += amount
   end
 
   def deduct(amount)
     raise "Insufficient Funds" if (amount > @balance)
-    @balance = @balance - amount
+    @balance -= amount
   end
 
   def error_message
     "ERROR!! The Maximum balance is £#{OysterCard::MAXIMUM_BALANCE}"
+  end
+
+  def balance_exceeds_limit?(amount)
+    (amount + @balance) > MAXIMUM_BALANCE
   end
 
 
